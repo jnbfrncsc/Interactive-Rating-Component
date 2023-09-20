8bfa4ad4122a0aa95b8228e9922294dae3ad810e
jnbfrncsc <jbfrncsc20@gmail.com>
2023-09-20 15:46:21 +0800

    Add Project Files

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const rates = document.querySelectorAll('.rates')
+const submitButton = document.querySelector('.btn_submit')
+const submitSection = document.querySelector('.thankSection')
+const ratingSection = document.querySelector('.ratingSection')
+let current_rating = document.querySelector('.ratesText')
+
+rates.forEach((rate, i) =>{
+    rate.onclick = function () {
+        let current_rate = i + 1;
+        current_rating.innerText = `You selected ${current_rate} out of 5`
+
+        rates.forEach(rate=>{
+            rate.classList.remove('active')
+        })
+        rate.classList.add('active')
+    }
+})
+
+submitButton.addEventListener('click', ()=>{
+    submitSection.classList.remove('sctHidden');
+    submitSection.classList.add('sctShow');
+    ratingSection.classList.add('sctHidden');
+})
